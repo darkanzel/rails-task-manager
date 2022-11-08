7beab92ff474f1e252c6704e5196e807d45cbcc6
Vinh Tho Hoang <vinhtho.hoang@gmail.com>
2022-11-08 14:58:58 -0500

    boolean added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
 end
